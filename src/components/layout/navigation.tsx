1c20221e8e27072762c45f3390fa17bf3ff0526d
its-darveloper <darveloper@gmail.com>
2025-02-23 21:57:15 -0500

    add event calendar

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -11,6 +11,7 @@ import { typography } from "@/lib/utils/typography"
 
 const navItems = [
   { path: "/faqs", label: "FAQs" },
+  { path: "https://createherfest.fyi/event-calendar", label: "Event Calendar" },
   { path: "https://tally.so/r/wzJkBE", label: "Speak" },
   { path: "https://tally.so/r/wQJJpp", label: "Mentor"},
   { path: "https://tally.so/r/wdjO1y", label: "Volunteer", isExternal: true },
